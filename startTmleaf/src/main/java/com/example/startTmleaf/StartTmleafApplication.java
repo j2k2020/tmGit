3d515a50b91d451d82b02865a2c2795720800549
Jaeeun Jeong <j2k2020@naver.com>
2021-08-25 21:48:07 +0900

    1st

---
+++ startTmleaf/src/main/java/com/example/startTmleaf/StartTmleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.startTmleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartTmleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartTmleafApplication.class, args);
+	}
+
+}
---
+++ startTmleaf/src/test/java/com/example/startTmleaf/StartTmleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startTmleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartTmleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
